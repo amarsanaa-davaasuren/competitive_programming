c4c5b8cb207d2f3b2f1b0d031d3ce0c3befc1c18
mrsn <ease.amaraa50@gmail.com>
2022-01-13 20:09:32 +0900

    fixed get_max

--- binary_trie.cpp
+++ binary_trie.cpp
@@ -54,174 +54,126 @@ int mod = 998244353;
 const ll INFLL = 1LL<<61;
 const int INF = 1<<30;
 
-comp inC(){
-    double x,y;
-    cin >> x >> y;
-    return {x,y};
-}
 
 
-
-void tle(){
-    rep(i,2002002002002) continue;
-}
-
-struct mint {
-    ll x; // typedef long long ll;
-    mint(ll x=0):x((x%mod+mod)%mod){}
-    mint operator-() const { return mint(-x);}
-    mint& operator+=(const mint a) {
-    if ((x += a.x) >= mod) x -= mod;
-        return *this;
-    }
-    mint& operator-=(const mint a) {
-    if ((x += mod-a.x) >= mod) x -= mod;
-        return *this;
-    }
-    mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
-    mint operator+(const mint a) const { return mint(*this) += a;}
-    mint operator-(const mint a) const { return mint(*this) -= a;}
-    mint operator*(const mint a) const { return mint(*this) *= a;}
-    mint pow(ll t) const {
-    if (!t) return 1;
-    mint a = pow(t>>1);
-    a *= a;
-    if (t&1) a *= *this;
-        return a;
-    }
-
-    // for prime mod
-    mint inv() const { return pow(mod-2);}
-    mint& operator/=(const mint a) { return *this *= a.inv();}
-    mint operator/(const mint a) const { return mint(*this) /= a;}
-};
-istream& operator>>(istream& is, mint& a) { return is >> a.x;}
-ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
-struct combination {
-vector<mint> fact, ifact;
-combination(int n):fact(n+1),ifact(n+1) {
-    assert(n < mod);
-    fact[0] = 1;
-    for (int i = 1; i <= n; ++i) fact[i] = fact[i-1]*i;
-    ifact[n] = fact[n].inv();
-    for (int i = n; i >= 1; --i) ifact[i-1] = ifact[i]*i;
-}
-mint operator()(int n, int k) {
-    if (k < 0 || k > n) return 0;
-    return fact[n]*ifact[k]*ifact[n-k];
-}
-} comb(100100);
-
-struct Node {
-    int val;
-    Node* next;
-    Node* prev;
-    Node(int val = 0) : val(val),next(nullptr), prev(nullptr) {}
-};
-
-
-struct Edge{    
-    int to,id;
-    Edge(int to, int id):to(to),id(id){}
-};
-
-
-
-struct binary_trie {
-    using val_type = int;
-    static constexpr int k = 8 * sizeof(val_type);
- 
-    struct Node {
-        unsigned int count = 0;
-        int idx[2] = {-1, -1};
+template<typename U = int, int B = 31>
+class binary_trie {
+    struct node {
+        int cnt;
+        node *ch[2];
+        node() : cnt(0), ch{ nullptr, nullptr } {}
     };
-    std::vector<Node> d{{}};
- 
-    void add(val_type x) {
-        int now = 0;
-        d[0].count++;
-        for(int i = k - 1; i >= 0; i--) {
-            bool bit = x >> i & 1;
-            if (d[now].idx[bit] == -1) {
-                d[now].idx[bit] = d.size();
-                d.emplace_back();
-            }
-            now = d[now].idx[bit];
-            d[now].count++;
-        }
+    node* add(node* t, U val, int b = B - 1) {
+        if (!t) t = new node;
+        t->cnt += 1;
+        if (b < 0) return t;
+        bool f = (val >> (U)b) & (U)1;
+        t->ch[f] = add(t->ch[f], val, b - 1);
+        return t;
     }
- 
-    val_type xor_max(val_type x) {
-        assert(d[0].count);
-        int now = 0;
-        val_type z = 0;
-        for(int i = k - 1; i >= 0; i--) {
-            bool bit = !(x >> i & 1);
-            if (d[now].idx[bit] == -1) bit = !bit;
-            z = z << 1 | bit;
-            now = d[now].idx[bit];
+    node* sub(node* t, U val, int b = B - 1) {
+        assert(t);
+        t->cnt -= 1;
+        if (t->cnt == 0) return nullptr;
+        if (b < 0) return t;
+        bool f = (val >> (U)b) & (U)1;
+        t->ch[f] = sub(t->ch[f], val, b - 1);
+        return t;
+    }
+
+    U get_min(node* t, U val, int b = B - 1) const {
+        assert(t);
+        if (b < 0) return 0;
+        bool f = (val >> (U)b) & (U)1;
+        bool next;
+        if (t->ch[f]) next = (f);
+        else next = (1^f);
+        f = (next^f);
+        return get_min(t->ch[next], val, b - 1) | ((U)f << (U)b);
+    }
+
+    U get_max(node* t, U val, int b = B - 1) const {
+        assert(t);
+        if (b < 0) return 0;
+        bool f = (val >> (U)b) & (U)1; 
+        bool next;
+        if (t->ch[1^f]) next = 1^f;
+        else next = f;
+        f = (next^f);
+        return get_max(t->ch[next], val, b - 1) | ((U)f << (U)b);
+    }
+
+    U get(node* t, int k, int b = B - 1) const {
+        if (b < 0) return 0;
+        int m = t->ch[0] ? t->ch[0]->cnt : 0;
+        return k < m ? get(t->ch[0], k, b - 1) : get(t->ch[1], k - m, b - 1) | ((U)1 << (U)b);
+    }
+    int count_lower(node* t, U val, int b = B - 1) {
+        if (!t || b < 0) return 0;
+        bool f = (val >> (U)b) & (U)1;
+        return (f && t->ch[0] ? t->ch[0]->cnt : 0) + count_lower(t->ch[f], val, b - 1);
+    }
+    node *root;
+public:
+    binary_trie() : root(nullptr) {}
+    int size() const {
+        return root ? root->cnt : 0;
+    }
+    bool empty() const {
+        return !root;
+    }
+    void insert(U val) {
+        root = add(root, val);
+    }
+    void erase(U val) {
+        root = sub(root, val);
+    }
+    U max_XOR_element(U bias = 0) const {
+        return get_max(root, bias);
+    }
+    U min_XOR_element(U bias = 0) const {
+        return get_min(root, bias);
+    }
+    int lower_bound(U val) { // return id
+        return count_lower(root, val);
+    }
+    int upper_bound(U val) { // return id
+        return count_lower(root, val + 1);
+    }
+    U operator[](int k) const {
+        assert(0 <= k && k < size());
+        return get(root, k);
+    }
+    int count(U val) const {
+        if (!root) return 0;
+        node *t = root;
+        for (int i = B - 1; i >= 0; i--) {
+            t = t->ch[(val >> (U)i) & (U)1];
+            if (!t) return 0;
         }
-        return x ^ z;
+        return t->cnt;
     }
 };
-
 
 void solve(){
-    int n,k;
-    cin >> n >> k;
+    int n = 100;
+    vector<int> a(n);
+    rep(i,n) a[i] = rand()%20;
+    SORT(a);
+    for(auto e:a) cout << e << " ";
+    cout << endl;
+    binary_trie bt;
+    for(auto e: a) bt.insert(e);
 
-    vector<ll> a(n);
-    rep(i,n) cin >> a[i];
-    unordered_map<ll,vector<ll>> mp;
-    
-    if (k==0){
-        cout << n << endl;
-        rep(i,n) cout << i + 1 << " "; 
-        return;
-    }
+    cout << "min: " << bt.min_XOR_element() << endl;
+    cout << "min we can get with xor with 21: " << bt.min_XOR_element(21) << endl;
+    cout << "max we can get with xor with 21: " << bt.max_XOR_element(21) << endl;
+    cout << "max: " << bt.max_XOR_element() << endl;
+    cout << "count 6: " << bt.count(6) << endl;
+    cout << "3-th value: " << bt[3] << endl;
+    cout << "lower bound 0: " << bt.lower_bound(0) << endl;
+    cout << "upper bound 0: " << bt.upper_bound(0) << endl;
 
-    int msb = 0;
-    rep(i,31) if ((k>>i)%2) msb = i;
-    rep(i,n){
-        mp[(a[i]>>(msb+1))].pb(i);
-    }
-
-    vector<int> ret;
-
-    for(auto& [val,values]:mp){
-        binary_trie bt;
-        for(auto id:values){
-            bt.add(a[id]);
-        }
-        bool f = 0;
-        for(auto id:values){
-            if (!f && bt.xor_max(a[id]) >= k){
-                f = 1;
-                ret.pb(id);
-                for(auto id2:values){
-                    if ((a[id]^a[id2]) >= k){
-                        ret.pb(id2);
-                        break;
-                    }
-                }
-            }
-        }
-        if (!f){
-            ret.pb(values[0]);
-        }
-    }
-
-    if (ret.size()<=1){
-        cout << -1 << endl;
-        return;
-    }
-
-    cout << ret.size() << endl;
-    for(auto e: ret){
-        cout << e + 1 << " ";
-    }
-    
-    
 
 }
 
